cee5ca6fa598f0a3d5d796252d6c7a7f460b1eed
h076 <hazawood@me.com>
2025-07-03 15:19:48 +0100

    added xtensor-blas.

--- linearModels/src/RegressionModel.hpp
+++ linearModels/src/RegressionModel.hpp
@@ -2,13 +2,30 @@
 #define REGRESSIONMODEL_H_
 
 #include <xtensor/containers/xarray.hpp>
+#include <xtensor-blas/xlinalg.hpp>
 
 class RegressionModel {
 
     protected:
         xt::xarray<double> X;
         xt::xarray<double> y;
+        xt::xarray<double> params; // model coefficients
+        xt::xarray<double> fittedValues; // The predicted values
+        xt::xarray<double> residuals; // residual error between true y and predicted
 
+    public:
+        RegressionModel(const xt::xarray<double>& x, const xt::xarray<double>& y)
+            : X(x), y(y) {};
+
+        virtual void fit() = 0;
+
+        xt::xarray<double> predict(const xt::xarray<double>& x) const {
+            return xt::linalg::dot(x, params);
+        }
+
+        xt::xarray<double> getParams() const {return params;}
+        xt::xarray<double> getFitted() const {return fittedValues;}
+        xt::xarray<double> getResiduals() const {return residuals;}
 };
 
 #endif // REGRESSIONMODEL_H_
